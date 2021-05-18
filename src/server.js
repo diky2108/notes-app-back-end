7b94a7598b5d18f04561b0b566ac583bbf47d3e5
Your Name <dwikyryan21@gmail.com>
2021-05-18 22:45:55 +0700

    initial commit

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.18.145',
+    host: 'localhost',
     routes: {
       cors: {
         origin: ['*'],
