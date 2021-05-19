09410499eb85bd53878ded9ff71f615c36e9bdc0
Dwiky <dwikyryan21@gmail.com>
2021-05-19 20:43:01 +0700

    fix bugs host value

--- src/handler.js
+++ src/handler.js
@@ -27,10 +27,9 @@ const addNoteHandler = (request, h) => {
     response.code(201);
     return response;
   }
-
   const response = h.response({
     status: 'fail',
-    message: 'Catatan gagal ditambhkan',
+    message: 'Catatan gagal ditambahkan',
   });
   response.code(500);
   return response;
@@ -92,7 +91,7 @@ const editNoteByIdHandler = (request, h) => {
 
   const response = h.response({
     status: 'fail',
-    message: 'Gagal diperbarui catatan, ID tidak ditemukan',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
   });
   response.code(404);
   return response;
